9e1807abcf9cf66fb28e86cb555a446bcfefe250
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2024-03-03 12:43:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,28 @@ async def consume():
     await consumer.start()
     print("consumer.start code is started!")
     try:
+        # cousumer 가 특정 토픽에 데이터가 들어오기전까지 무한정 대기하는 현상
         async for msg in consumer:
             print("consumed: ", msg.topic, msg.partition, msg.offset, msg.key, msg.value, msg.timestamp)
     except Exception as e:
         print(e)
         return {"response": e}
     return {"response": "consume success from kafka"}
+
+
+@app.get("/kafka-consume")
+async def consume():
+    print("consumer started!")
+    consumer = AIOKafkaConsumer("foobar", "foobar1", bootstrap_servers='localhost:9092', group_id="123",
+                                request_timeout_ms=40000)
+    await consumer.start()
+    print("consumer.start code is started!")
+    try:
+        # timeout 시간동안 대기후 wake up
+        msg = await asyncio.wait_for(consumer.getone(),timeout=5)
+        print(msg.topic, msg.offset, msg.key, msg.value)
+    except Exception as e:
+        print(e)
+    finally:
+        await consumer.stop()
+        return {"response": "consume success from kafka"}
